177572f82a9200d28d9871a62a5b0a5f1bd222a7
macdja38 <jakeincanada@icloud.com>
2017-10-08 02:49:32 -0400

    Create documentation and npm ignore

---
+++ docma.js
@@ -0,0 +1,73 @@
+/**
+ * Created by macdja38 on 2017-02-28.
+ */
+"use strict";
+const Docma = require('docma');
+
+let config = {
+  app: {
+    title: 'minecraft-jsonapi',
+    base: '/minecraft-jsonapi/',
+    server: Docma.ServerType.GITHUB,
+  },
+  src: [
+    'functionalIndex.js',
+    './README.md',
+  ],
+  dest: './docs',
+  jsdoc: {
+    plugins: []
+  },
+  template: {
+    // Template-specific options
+    options: {
+      title: 'Minecraft-JSONapi',
+      sidebar: true,
+      collapsed: false,
+      badges: true,
+      search: true,
+      navbar: true,
+      navItems: [
+        {
+          label: 'Documentation',
+          href: '#',
+          iconClass: 'ico-book'
+        },
+        {
+          label: 'Demos',
+          href: 'https://github.com/macdja38/pvpcraft/tree/master/modules',
+          iconClass: 'ico-mouse-pointer'
+        },
+        {
+          label: 'Download',
+          iconClass: 'ico-md ico-download',
+          items: [
+            {
+              label: 'dev',
+              href: 'https://github.com/macdja38/minecraft-jsonapi/archive/dev.zip',
+            },
+            {
+              label: 'release',
+              href: 'https://github.com/macdja38/minecraft-jsonapi/archive/master.zip',
+            }
+          ]
+        },
+        {
+          label: 'GitHub',
+          href: 'https://github.com/macdja38/minecraft-jsonapi',
+          target: '_blank',
+          iconClass: 'ico-md ico-github'
+        }
+      ]
+    }
+  },
+};
+
+Docma.create()
+  .build(config)
+  .then(function () {
+    console.log('Documentation built successfully.');
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
--- functionalIndex.js
+++ functionalIndex.js
@@ -2,13 +2,34 @@ const crypto = require("crypto");
 const request = require("request-promise-native");
 const WebSocket = require("ws");
 
+/**
+ * Options involving which server to connect to and how to connect
+ * @typedef {Object} Options
+ * @property {string} [host=localhost] hostname of server to connect to
+ * @property {number} [port=25565] port of minecraft server to connect to
+ * @property {boolean} [https=false] use https to connect
+ * @property {string} username username to connect with (for jsonapi defined in your server's config, not your minecraft account)
+ * @property {string} password password to connect with.
+ */
 const defaults = {
   host: "localhost",
-  port: 20059,
+  port: 25565,
   https: false,
 };
 
+/**
+ * The api
+ * @typedef {Object} MinecraftJSONapi
+ * @property {function} add {@link #add|add}
+ * @property {function} follow {@link #follow|follow}
+ * @property {function} dispatch {@link #dispatch|dispatch}
+ */
+
 module.exports = {
+  /**
+   * Creates a request object
+   * @returns {MinecraftJSONapi} {@link #MinecraftJSONapi|MinecraftJSONapi}
+   */
   createRequest: function () {
     return {
       add: add.bind(null, []),
@@ -21,6 +42,13 @@ module.exports = {
   },
 };
 
+/**
+ * Adds a parameter to the list of things to be fetched
+ * @typedef {function} add
+ * @param {String} name {@link http://mcjsonapi.com/apidocs/|Method Name}
+ * @param {Array} args
+ * @returns {MinecraftJSONapi} {@link #MinecraftJSONapi|MinecraftJSONapi}
+ */
 function add(requests, name, args = []) {
   const duplicatedRequests = requests.slice(0);
   duplicatedRequests.push({name, args});
@@ -31,6 +59,12 @@ function add(requests, name, args = []) {
   }
 }
 
+/**
+ * Sends request to the api server
+ * @typedef {function} dispatch
+ * @param {Options} options {@link #Options|Options}
+ * @returns {Array<Object>} Array of {@link http://mcjsonapi.com/#json-response-structure|JSONapi Responses}
+ */
 function dispatch(requests, options = {}) {
   const optionsWithDefaults = Object.assign({}, defaults, options);
 
@@ -67,6 +101,12 @@ function generateKey(username, name, password) {
   return crypto.createHash("sha256").update(username + name + password, "utf-8").digest("hex")
 }
 
+/**
+ * Uses a websocket connection to monitor for changes in a request, only certain {@link https://github.com/alecgorge/jsonapi/wiki/Stream-sources|methods} are supported
+ * @param {Options} options {@link #Options|Options}
+ * @param {function} callback will be called with each event
+ * @returns {WebSocket}
+ */
 function follow(requests, options, callback) {
   const optionsWithDefaults = Object.assign({}, defaults, options);
 
